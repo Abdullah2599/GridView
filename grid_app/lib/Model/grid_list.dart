aa438686c54fe0dd140d62360abe01af873acade
mbilal2024 <abdullahcloudupload@gmail.com>
2024-05-18 11:01:07 +0500

    Update grid_list.dart

--- grid_app/lib/Model/grid_list.dart
+++ grid_app/lib/Model/grid_list.dart
@@ -2,5 +2,6 @@
 import 'package:grid_app/Controller/grid_controller.dart';
 
 List<GridModel> get=[
-  GridModel(name: 'Abc', price: '250', link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBB5po-GLvpbBVqjfx15evCqN7lxROzcn-TsyxfqrLJg&s',)
+  GridModel(name: 'Abc', price: '250', link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBB5po-GLvpbBVqjfx15evCqN7lxROzcn-TsyxfqrLJg&s',),
+   GridModel(name: 'Abc', price: '250', link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBB5po-GLvpbBVqjfx15evCqN7lxROzcn-TsyxfqrLJg&s',)
 ];

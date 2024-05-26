435bf66c33e9c81b44255f05ce0eeb454535a6a1
Abdullah2599 <abdullahcloudupload@gmail.com>
2024-05-26 09:16:07 +0500

    updated

--- grid_app/lib/Calculator/main_page.dart
+++ grid_app/lib/Calculator/main_page.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:expressions/expressions.dart';
 
 class MainView extends StatefulWidget {
   const MainView({super.key});
 
   @override
-  State<MainView> createState() => _MianViewState();
+  State<MainView> createState() => _MainViewState();
 }
 
-class _MianViewState extends State<MainView> {
+class _MainViewState extends State<MainView> {
   final List<String> buttons = [
     '7',
     '8',
@@ -25,7 +26,28 @@ class _MianViewState extends State<MainView> {
     '.',
     'C',
     '/',
+    '='
   ];
+
+  String input = '';
+  String result = '';
+
+  void buttonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == 'C') {
+        input = '';
+        result = '';
+      } else if (buttonText == '=') {
+        input = input.replaceAll('x', '*');
+        final expression = Expression.parse(input);
+        const evaluator = ExpressionEvaluator();
+        final value = evaluator.eval(expression, {});
+        result = value.toString();
+      } else {
+        input += buttonText;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,52 +63,61 @@ class _MianViewState extends State<MainView> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
+            padding: const EdgeInsets.all(20),
             height: 140,
-            width: 70,
             color: Colors.amber,
-            child: const Center(
-              child: Text(
-                "Text Goes Here",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 40,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  input,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                  ),
                 ),
-              ),
+                Text(
+                  result,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 40,
+                  ),
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            padding: const EdgeInsets.all(8.0),
-            height: 330,
-            // width: 180,
-            child: GridView.builder(
-              itemCount: 16,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 8,
-                childAspectRatio: 1.20,
-              ),
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        buttons[index],
-                        style: const TextStyle(fontSize: 15),
+          const SizedBox(height: 20),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.builder(
+                itemCount: buttons.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 8,
+                  childAspectRatio: 1.20,
+                ),
+                itemBuilder: (context, index) {
+                  final buttonText = buttons[index];
+                  return GestureDetector(
+                    onTap: () => buttonPressed(buttonText),
+                    child: Container(
+                      child: Center(
+                        child: Text(
+                          buttonText,
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: buttonText == '=' ? Colors.green : Colors.red,
                       ),
                     ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.red,
-                    ),
-                    height: 20,
-                    width: 20,
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
           ),
         ],
